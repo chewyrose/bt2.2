2a80f59910f3e3e21a8e20604b76befba80f9992
chewyrose <hn0192306@gmail.com>
2024-10-04 21:01:07 +0700

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,17 @@
+public class Book {
+    private String name;
+    private double price;
+    private Author author;
+    Book(String name, double price){
+        this.name = name;
+        this.price = price;
+        this.author = author;
+    }
+    String getName(){
+        return name;
+    }
+
+
+
+
+}
